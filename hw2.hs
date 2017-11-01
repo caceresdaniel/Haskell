eacc5ae1773e8839b71100c1d9dd11ce92423932
caceresdaniel <mattmoocow@live.com>
2017-11-01 00:26:04 -0700

    did question 3

--- hw2.hs
+++ hw2.hs
@@ -1,8 +1,22 @@
+-- 1)
 isDivBy5 :: Int -> Bool
 isDivBy5 x  = x `mod` 5 == 0
 
+-- 2)
 everyOther :: String -> [Bool]
 everyOther [] = [True]
 everyOther (x:[]) = [( x `elem` ['a' .. 'f'])]
 everyOther (x:y:xs) = ( x `elem` ['a' .. 'f']) : everyOther xs
+
+-- 3)
+zipMapAdd :: [Int] -> [Int] -> [Int]
+zipMapAdd x y = mapZipAdd (zip x y)
+
+mapZipAdd :: [(Int, Int)] -> [Int]
+mapZipAdd [] = []
+mapZipAdd ((x, y):rest) = x+y : mapZipAdd rest
+
+-- 4)
+mapString :: [String] -> [String]
+mapString [] = []
 
